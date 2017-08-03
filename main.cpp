3938d7523f7d57eca684730cadaf344129527291
kasrasadeghi <kasra.sadeghi1@gmail.com>
2017-08-03 17:07:40 -0500

    Previous directory tracking and rendering fix. Update for integration with VS Code.

--- main.cpp
+++ main.cpp
@@ -50,6 +50,7 @@ class User {
     _entries.clear();
     directory_iterator iter(current_path());
     std::copy(begin(iter), end(iter), back_inserter(_entries));
+    std::sort(_entries.begin(), _entries.end());
     _reset_cursorline();
   }
 
@@ -63,38 +64,52 @@ public:
   }
   
   void display() {
+    move(1, 0);
     printer p;
-    
-    if (_cursorline == 0) attron(A_BOLD);
-    p << current_path().string() << "\n";
-    attroff(A_BOLD);
 
-    size_t size = _entries.size();
-    
-    for (unsigned int i = 0; i < size; ++i) {
-      const auto& v = _entries[i];
-      if (i + 1 == size)
-        break;
-      p << "├── ";
-      if (_cursorline - 1 == i)
+    try {
+      if (_cursorline == 0) attron(A_BOLD);
+      p << current_path().string() << "\n";
+      attroff(A_BOLD);
+      
+      size_t size = _entries.size();
+      
+      for (unsigned int i = 0; i < size; ++i) {
+        const auto& v = _entries[i];
+        if (i + 1 == size)
+          break;
+        p << "├── ";
+        if (_cursorline - 1 == i)
+          attron(A_BOLD);
+        p << v.path().filename().string();
+        if (is_directory(v)) p << "/";
+        attroff(A_BOLD);
+        p << "\n";
+      }
+      p << "└── ";
+      if (_cursorline == size)
         attron(A_BOLD);
-      p << v.path().filename().string();
-      if (is_directory(v)) p << "/";
+      p << _entries[size - 1].path().filename().string();
+      if (is_directory(_entries[size - 1])) p << "/";
       attroff(A_BOLD);
       p << "\n";
+    } catch(const filesystem_error& err) {
+      addstr(err.what() + '\n');
     }
-    p << "└── ";
-    if (_cursorline == size)
-      attron(A_BOLD);
-    p << _entries[size - 1].path().filename().string();
-    attroff(A_BOLD);
-    p << "\n";
+    
   }
 
   void folder_up() {
+    path prev_wd = current_path();
     chdir("..");
     _reset_entries();
-    
+    size_t size = _entries.size();
+    for (size_t i = 0; i < size; ++i) {
+      if (_entries[i] == prev_wd) {
+        _cursorline = i + 1;
+        break;
+      }
+    }
   }
 
   void folder_down() {
@@ -118,51 +133,78 @@ public:
 
 int main(int argc, char** argv) {
   setlocale(LC_ALL,"");
-  initscr();
-  noecho();
-  keypad(stdscr, TRUE);
-  raw();
+  initscr();             // initialize ncurses
+  noecho();              // don't actually write the characters received from input
+  keypad(stdscr, TRUE);  // special characters like F1
+  raw();                 // print things immediately
+  curs_set(0);           // turn off cursor
   bool done = false;
   printer p;  
-  User user;
-  
-  while (!done) {
+  User user; 
+  int c = 0;
+
+  user.display();
+
+  do {
+    c = getch();
     clear();
-    move(0, 0);
-
-    try {
-      user.display();
-    } catch (const filesystem_error& ex) {
-      addstr(ex.what() + '\n');
-    }
     
-    int c = getch();
-
-    move(15, 0);
-    p << "input code: " << c << "\n";
-
+    move(0, 0);
+    p << "input code: " << c << ", ";
+    
+    move(0, 17);
     switch(c) {
     case 113: //q
       done = true;
       break;
-    case 259: // arrow up
-      p << "UP\n";
-      user.cursor_up();
+
+    case 115: // s - search query
+      p << "SEARCH QUERY (unimplemented)\n";
       break;
+      
+    case 10:  // enter - search close
+      p << "SEARCH CLOSE (unimplemented)\n";
+      break;
+      
+    case 102: // f - history forward
+      p << "HISTORY FORWARD (unimplemented)\n";
+      break;
+      
+    case 98:  // b - history back
+      p << "HISTORY BACK (unimplemented)\n";
+      break;
+      
+      // navigation
+    case 104: // h
+    case 260: // arrow left
+      p << "FOLDER UP\n";
+      user.folder_up();
+      break;
+
+    case 106: // j
     case 258: // arrow down
       p << "DOWN\n";
       user.cursor_down();
       break;
-    case 260: // arrow left
-      user.folder_up();
+      
+    case 107: // k
+    case 259: // arrow up
+      p << "UP\n";
+      user.cursor_up();
       break;
+      
+      
+    case 108:  // l
     case 261: // arrow right
+      p << "FOLDER DOWN\n";
       user.folder_down();
       break;
     }
-
+    
+    user.display();
     refresh();
-  }
+    
+  } while (!done);
 
   endwin();
 }
